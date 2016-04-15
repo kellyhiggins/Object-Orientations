77f5c8e9e78ff51a777dfb285fccc0f2faab36b8
Hackbright Student <info@hackbrightacademy.com>
2016-04-15 13:04:46 -0700

    updated get_total method,  reorganized subclasses

--- melons.py
+++ melons.py
@@ -15,10 +15,16 @@ class AbstractMelonOrder(object):
         """Calculate price."""
 
         base_price = 5
+        
         if species == 'Christmas melon':
-            total = (1 + self.tax) * self.qty * base_price * 1.5
-        else:
-            total = (1 + self.tax) * self.qty * base_price
+            base_price *= 1.5
+        
+        total = (1 + self.tax) * self.qty * base_price
+
+        if order_type == 'international' and qty > 10:
+            total += 3
+
+        
         return total
 
     def mark_shipped(self):
@@ -29,11 +35,9 @@ class AbstractMelonOrder(object):
 
 class DomesticMelonOrder(AbstractMelonOrder):
     """A domestic (in the US) melon order."""
+    
     def __init__(self, species, qty):
-        """Initialize melon order attributes"""
-
-        self.order_type = "domestic"
-        self.tax = 0.08
+        super(DomesticMelonOrder, self).__init__(species, qty, 'domestic', 0.08)
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
@@ -42,8 +46,9 @@ class InternationalMelonOrder(AbstractMelonOrder):
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes"""
 
-        super(InternationalMelonOrder, self).__init__(species, qty, "international", 0.08)
+        super(InternationalMelonOrder, self).__init__(species, qty, 'international', 0.17)
         self.country_code = country_code
+
 
     def get_country_code(self):
         """Return the country code."""
